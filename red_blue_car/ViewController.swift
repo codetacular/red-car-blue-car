9a97327aa86a41a8301b420632260053eb46de71
user903031 <user903031@ny083.local>
2016-09-17 16:02:05 -0400

    Initial Commit

---
+++ red_blue_car/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  red_blue_car
+//
+//  Created by user903031 on 9/17/16.
+//  Copyright © 2016 com.proobject.red_blue_car. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
